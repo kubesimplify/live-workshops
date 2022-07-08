b5dbbb61b45d600063cebeedab225218e5f8788c
Kunal Verma <vkunal321@gmail.com>
2022-07-08 12:35:57 +0530

    Add: GitOps workshop

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 var btnWorshop1 = document.getElementById("work-1");
 var btnWorshop2 = document.getElementById("work-2");
 var btnWorshop3 = document.getElementById("work-3");
+var btnWorshop4 = document.getElementById("work-4");
 
 // __________________________________________________________________
 
@@ -19,5 +20,5 @@ btnWorshop2.addEventListener("click", function (e) {
 btnWorshop3.addEventListener("click", function (e) {
   e.preventDefault();
 
-  location.href = "#";
+  location.href = "https://forms.gle/prWQQhWxwh88cnqo8";
 });
