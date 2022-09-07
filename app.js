0720aa9cbfe7c2f84869bd2d6cad83d4de0bbcde
Pavan Gudiwada <25551553+pavangudiwada@users.noreply.github.com>
2022-09-07 11:04:02 +0530

    workshops 8 and 9

--- app.js
+++ app.js
@@ -1,61 +1,61 @@
 // Bringing the register buttons from HTML
 
-var btnWorshop1 = document.getElementById("work-1");
-var btnWorshop2 = document.getElementById("work-2");
-var btnWorshop3 = document.getElementById("work-3");
-var btnWorshop4 = document.getElementById("work-4");
-var btnWorshop5 = document.getElementById("work-5");
-var btnWorshop6 = document.getElementById("work-6");
-var btnWorshop7 = document.getElementById("work-7");
-var btnWorshop8 = document.getElementById("work-8");
-var btnWorshop9 = document.getElementById("work-9");
+var btnWorshop1 = document.getElementById("work-1")
+var btnWorshop2 = document.getElementById("work-2")
+var btnWorshop3 = document.getElementById("work-3")
+var btnWorshop4 = document.getElementById("work-4")
+var btnWorshop5 = document.getElementById("work-5")
+var btnWorshop6 = document.getElementById("work-6")
+var btnWorshop7 = document.getElementById("work-7")
+var btnWorshop8 = document.getElementById("work-8")
+var btnWorshop9 = document.getElementById("work-9")
 
 // __________________________________________________________________
 
 btnWorshop1.addEventListener("click", function (e) {
-  e.preventDefault();
+  e.preventDefault()
 
-  window.open("https://youtu.be/EUu1E_YKGTw", "_blank");
-});
+  window.open("https://youtu.be/EUu1E_YKGTw", "_blank")
+})
 btnWorshop2.addEventListener("click", function (e) {
-  e.preventDefault();
+  e.preventDefault()
 
-  window.open("https://youtu.be/PN3VqbZqmD8", "_blank");
-});
+  window.open("https://youtu.be/PN3VqbZqmD8", "_blank")
+})
 btnWorshop3.addEventListener("click", function (e) {
-  e.preventDefault();
+  e.preventDefault()
 
-  window.open("https://youtu.be/5rwIIusbUWM", "_blank");
-});
+  window.open("https://youtu.be/5rwIIusbUWM", "_blank")
+})
 btnWorshop4.addEventListener("click", function (e) {
-  e.preventDefault();
+  e.preventDefault()
 
-  window.open("https://youtu.be/ka0C09CAfho", "_blank");
-});
+  window.open("https://youtu.be/ka0C09CAfho", "_blank")
+})
 btnWorshop5.addEventListener("click", function (e) {
-  e.preventDefault();
+  e.preventDefault()
 
-  window.open("https://forms.gle/Zds9Sc9jdRXAxZeQ8", "_blank");
-});
+  window.open("https://forms.gle/Zds9Sc9jdRXAxZeQ8", "_blank")
+})
 btnWorshop6.addEventListener("click", function (e) {
-  e.preventDefault();
+  e.preventDefault()
 
-  window.open("https://youtu.be/259I52_Zh3E", "_blank");
-});
+  window.open("https://youtu.be/259I52_Zh3E", "_blank")
+})
 btnWorshop7.addEventListener("click", function (e) {
-  e.preventDefault();
+  e.preventDefault()
 
-  window.open("https://youtu.be/9KC4Jgtz_j4", "_blank");
-});
+  window.open("https://youtu.be/9KC4Jgtz_j4", "_blank")
+})
 
 // new workshops here:
 btnWorshop8.addEventListener("click", function (e) {
-  e.preventDefault();
+  e.preventDefault()
 
-  window.open("#", "_blank");
-});
+  window.open("https://forms.gle/ygjNXC8bB7PmNGmXA", "_blank")
+})
 btnWorshop9.addEventListener("click", function (e) {
-  e.preventDefault();
+  e.preventDefault()
 
-  window.open("#", "_blank");
-});
+  window.open("https://forms.gle/mexwhLULBaQKywxJ6", "_blank")
+})
